9dcf671a0068c109130a23bc1e6acb5521ccec44
Miguel Villalba Paez <villalbapaezmiguel@gmail.com>
2021-02-01 08:11:28 -0300

    segundo cambio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Miguel");
+	alert("Hola Miguel , segundo cambio");
 }
 
